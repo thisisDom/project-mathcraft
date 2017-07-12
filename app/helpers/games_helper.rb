b143f5e0693fa72786263e480d6ab7f6edf1093f
Roger <lakersli@gmail.com>
2017-07-12 08:03:14 -0700

    Integrate levels API call into level-select screen

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,4 +3,9 @@ module GamesHelper
   def get_questions(type, difficulty)
     return HTTParty.get("http://mathcraft-api.herokuapp.com/generate/?type=#{type}&difficulty=#{difficulty}")['questions']
   end
+
+  def get_levels
+    return HTTParty.get("http://mathcraft-api.herokuapp.com/levels")['levels']
+  end
+
 end
