cee543f11163d4da57d1dd7cc94517cf27bb9227
Roger <lakersli@gmail.com>
2017-07-13 13:32:40 -0700

    Fix ability to gather stone and remove questions class and migration

--- db/migrate/20170711172836_create_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestions < ActiveRecord::Migration[5.0]
-  def change
-    create_table :questions do |t|
-      t.string :title
-      t.string :answer
-      t.timestamps
-    end
-  end
-end
